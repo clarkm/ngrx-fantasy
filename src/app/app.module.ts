10c0b76a7fecc877b29f453924be85c56b63ba12
Clark Mercer <clarkmercer@gmail.com>
2021-05-20 10:04:27 -0400

    add redux store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,15 @@ import { draftPoolReducer } from './reducers/draft-pool.reducer';
 import { TeamsComponent } from './teams/teams.component';
 import { DraftPoolComponent } from './draft-pool/draft-pool.component';
 import { CurrentTeamService } from './current-team.service';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  imports: [BrowserModule, StoreModule.forRoot({teams: teamsReducer, draftPool: draftPoolReducer})],
+  imports: [BrowserModule, StoreModule.forRoot({teams: teamsReducer, draftPool: draftPoolReducer}),
+  // Instrumentation must be imported after importing StoreModule (config is optional)
+  StoreDevtoolsModule.instrument({
+    maxAge: 25, // Retains last 25 states
+    // logOnly: environment.production, // Restrict extension to log-only mode
+  }) ],
   declarations: [AppComponent, TeamsComponent, DraftPoolComponent],
   bootstrap: [AppComponent],
   providers: [CurrentTeamService],
